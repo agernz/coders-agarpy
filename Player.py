bf2964259cfc61a7562aa71dacb2f5ebb2ff75f9
agernz <agernz@gmail.com>
2020-06-07 23:01:09 -0500

    fix update, remove sprite use, added draw, slow down with mass, and basic movement commands

--- Player.py
+++ Player.py
@@ -1,43 +1,62 @@
-from constants import pygame, DISPLAY_WIDTH, DISPLAY_HEIGHT, COLORKEY
+from constants import pg, DISPLAY_WIDTH, DISPLAY_HEIGHT
 
 
-class Player(pygame.sprite.Sprite):
+class Player():
 
-    def __init__(self, init_x, init_y, color):
-        pygame.sprite.Sprite.__init__(self)
+    def __init__(self, init_x, init_y, color, name):
         self.cur_state = (init_x, init_y)
         self.prev_state = self.cur_state
         self.color = color
-        self.radius = 30
-        self.x_velocity = 0
-        self.y_velocity = 0
+        self.radius = 10
+        self.x_dir = 0
+        self.y_dir = 0
+        self.speed = 9
+        self.velocity = self.speed
         self.image = None
         self.rect = None
-        self.draw_circle()
+        font = pg.font.SysFont('chalkduster.ttf', 20)
+        self.name_text = font.render(name, True, (255, 255, 255))
 
-    def draw_circle(self):
-        self.image = pygame.Surface((self.radius * 2, self.radius * 2))
-        self.image.fill(COLORKEY)
-        self.image.set_colorkey(COLORKEY)
-        self.rect = pygame.draw.circle(self.image, self.color, \
-            (self.radius, self.radius), self.radius)
+    def increase_size(self, delta):
+        self.radius += delta
+        if self.velocity > 1:
+            self.velocity = round(self.speed - self.radius / 30.)
 
-    def increase_size(self, radius_delta):
-        self.radius += radius_delta
-        self.draw_circle()
+    def move(self, direciton):
+        if direciton == 'UP':
+            self.y_dir = 1
+        elif direciton == 'DOWN':
+            self.y_dir = -1
+        if direciton == 'RIGHT':
+            self.x_dir = 1
+        elif direciton == 'LEFT':
+            self.x_dir = -1
 
-    def update(self, alpha):
-        if alpha != -1:
-            a = tuple(x * alpha for x in self.cur_state)
-            b = tuple(x * (1.0 - alpha) for x in self.prev_state)
-            inter_state = (a[0] + b[0], a[1] + b[1])
-            self.rect.center = inter_state
-        else:
-            self.prev_state = self.cur_state
-            self.cur_state = (self.cur_state[0] + self.x_velocity, \
-                self.cur_state[1] + self.y_velocity)
-            if self.rect.top < 0 or self.rect.bottom > DISPLAY_HEIGHT:
-                self.cur_state = (self.cur_state[0], self.prev_state[1])
-            if self.rect.left < 0 or self.rect.right > DISPLAY_WIDTH:
-                self.cur_state = (self.prev_state[0], self.cur_state[1])
-            self.rect.center = self.cur_state
+    def stop(self):
+        self.x_dir = 0
+        self.y_dir = 0
+
+    def interpolate(self, alpha):
+        a = tuple(x * alpha for x in self.cur_state)
+        b = tuple(x * (1.0 - alpha) for x in self.prev_state)
+        inter_state = (a[0] + b[0], a[1] + b[1])
+        self.rect.center = inter_state
+
+    def update(self):
+        self.prev_state = self.cur_state
+        self.cur_state = (self.cur_state[0] + self.x_dir * self.velocity, \
+            self.cur_state[1] + self.y_dir * self.velocity)
+        if self.cur_state[1] < 0 or self.cur_state[1] > DISPLAY_HEIGHT:
+            self.cur_state = (self.cur_state[0], self.prev_state[1])
+        if self.cur_state[0] < 0 or self.cur_state[0] > DISPLAY_WIDTH:
+            self.cur_state = (self.prev_state[0], self.cur_state[1])
+        self.rect.center = self.cur_state
+
+    def draw(self, target_surface):
+        self.rect = pg.draw.circle(target_surface, self.color, \
+            self.cur_state, self.radius)
+        text_pos = (
+            self.cur_state[0] - self.name_text.get_width() / 2,
+            self.cur_state[1] - self.name_text.get_height() / 2
+        )
+        target_surface.blit(self.name_text, text_pos)
