2b9e2b2938a797f504a5971e2323143d56e7b8d4
agernz <agernz@gmail.com>
2020-06-07 23:02:10 -0500

    added simple class for dots player collects for points

---
+++ Blob.py
@@ -0,0 +1,26 @@
+import random as rand
+from constants import pg
+
+BLOB_TIERS = {
+    1: ((255, 46, 46), 5, 5),
+    2: ((18, 255, 18), 3, 4),
+    3: ((0, 196, 196), 1, 3)
+}
+
+
+class Blob():
+
+    def __init__(self, init_x, init_y):
+        self.rect = None
+        self.pos = (init_x, init_y)
+        self.tier = rand.randrange(100)
+        if self.tier < 10:
+            self.color, self.points, self.radius = BLOB_TIERS[1]
+        elif self.tier < 20:
+            self.color, self.points, self.radius = BLOB_TIERS[2]
+        else:
+            self.color, self.points, self.radius = BLOB_TIERS[3]
+
+    def draw(self, target_surface):
+        self.rect = pg.draw.circle(target_surface, self.color, \
+            self.pos, self.radius)
