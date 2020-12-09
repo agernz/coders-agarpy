71677d04847c5c3fec1973da9b1cf74c7929dffc
agernz <agernz@gmail.com>
2020-12-08 22:46:30 -0600

    remove uneened values

--- constants.py
+++ constants.py
@@ -3,18 +3,5 @@ with contextlib.redirect_stdout(None):
     import pygame as pg
 pg.init()
 
-DISPLAY_WIDTH = 1280
-DISPLAY_HEIGHT = 960
-ENV = 'dev'
-
-PLAYER_COLORS = {
-    'RED': (208, 0, 0),
-    'YELLOW': (255, 224, 102),
-    'BLUE': (36, 123, 160),
-    'MINT': (112, 193, 179),
-    'ORANGE': (235, 94, 40),
-    'GREEN': (63, 163, 77),
-    'PINK': (255, 166, 193)
-}
-BACKGROUND_COLOR = (255, 236, 209)
-TEXT_COLOR = (0, 0, 0)
+DISPLAY_WIDTH = 640
+DISPLAY_HEIGHT = 320
